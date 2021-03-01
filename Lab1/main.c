f55ecf9996c9a39d23755981edfd40a3b1ffc740
Romolik <romanSO3@mail.ru>
2021-03-01 15:32:33 +0700

    version 1

--- Lab1/main.c
+++ Lab1/main.c
@@ -2,69 +2,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/time.h>
 
-void mulMatrix (double *A, double *x, double *condition, const size_t N) {
-	for (size_t i = 0; i < N; ++i) {
+struct timeval tv1, tv2;
+
+void mulMatrix (double *A, double *x, double *condition,
+				const size_t N, const size_t M, size_t rank) {
+
+	for (size_t i = 0; i < M; ++i) {
 		double cur = 0;
 		for (size_t j = 0; j < N; ++j) {
-			cur += (A[i * N + j] * x[j]);
+			cur += (A[i * N + j] * x[rank * M + j]);
 		}
-		condition[i] = cur;
+		condition[rank * M + i] = cur;
 	}
 }
 
-void difCol (double *Ax, double *b, const size_t N) {
-	for (size_t i = 0; i < N; ++i) {
-		Ax[i] -= b[i];
+void difCol (double *Ax, double *b, const size_t M, size_t rank) {
+	for (size_t i = 0; i < M; ++i) {
+		Ax[i] -= b[rank * M + i];
 	}
 }
 
-double normCounting (double *Ax, const size_t N) {
+double normCounting (double *Ax, const size_t M) {
 	double res = 0;
-	for (size_t i = 0; i < N; ++i) {
+	for (size_t i = 0; i < M; ++i) {
 		res += (Ax[i] * Ax[i]);
 	}
-	return sqrt (res);
+	double norm;
+	MPI_Allreduce(&res, &norm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+	return sqrt (norm);
 }
 
-void mulScalarCol (double *Ax, const double scalar, const size_t N) {
-	for (size_t i = 0; i < N; ++i) {
+double normCountingForB (double *b, const size_t M, size_t rank, size_t N) {
+	double res = 0;
+	for (size_t i = rank * M; i < (rank * M + M * N); ++i) {
+		res += (b[i] * b[i]);
+	}
+	double norm;
+	MPI_Allreduce(&res, &norm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+	return sqrt (norm);
+}
+
+void mulScalarCol (double *Ax, const double scalar,
+				   const size_t M) {
+	for (size_t i = 0; i < M; ++i) {
 		Ax[i] *= scalar;
 	}
 }
 
-void printMatrix (double *A, const size_t N) {
+void printCol (double *x, const size_t N) {
 	for (size_t i = 0; i < N; ++i) {
-		for (size_t j = 0; j < N; ++j) {
-			printf ("%f\t", A[i * N + j]);
-		}
-		printf ("\n");
+		printf ("%f\n", x[i]);
 	}
 }
 
-void printCol (double *Ax, const size_t N) {
-	for (size_t i = 0; i < N; ++i) {
-		printf ("%f\n", Ax[i]);
-	}
-}
-
-int criterionCompletion (
-	double *Ax, double *b, const size_t N,
-	const double EPSILON) {
-	difCol (Ax, b, N);
-	double firsrNorm = normCounting (Ax, N);
-	double secondNorm = normCounting (b, N);
+int criterionCompletion (double *Ax, double *b, const size_t M, size_t rank,
+						 const size_t N, const double EPSILON) {
+	difCol (Ax, b, M, rank);
+	double firsrNorm = normCounting (Ax, M);
+	double secondNorm = normCountingForB( b, M, rank, N);
 	return (firsrNorm / secondNorm) >= EPSILON;
 }
 
-void simpleIterationMethod (
-	double *A, double *b, double *x,
-	const size_t N, const double EPSILON) {
+void simpleIterationMethod (double *A, double *b, double *x, const size_t N,
+							const size_t M, size_t rank, const double EPSILON) {
 	const double t = 0.0001;
 	double *condition = NULL;
-	condition = (double *) malloc (N * sizeof (double));
+	condition = (double *)malloc (N * sizeof (double));
 	double *Ax = NULL;
-	Ax = (double *) malloc (N * sizeof (double));
+	Ax = (double *)malloc (M * sizeof (double));
 
 	if (condition == NULL || Ax == NULL) {
 		free (Ax);
@@ -73,42 +80,41 @@ void simpleIterationMethod (
 		return;
 	}
 
-	mulMatrix (A, x, condition, N);
+	mulMatrix (A, x, condition, N, M, rank);
 
-	while (criterionCompletion (condition, b, N, EPSILON)) {
-		mulMatrix (A, x, Ax, N);
-		difCol (Ax, b, N);
-		mulScalarCol (Ax, t, N);
-		difCol (x, Ax, N);
-		mulMatrix (A, x, condition, N);
+	while (criterionCompletion (condition, b, M, rank, N, EPSILON)) {
+		mulMatrix (A, x, Ax, N, M, rank);
+		difCol (Ax, b, M, rank);
+		mulScalarCol (Ax, t, M);
+		difCol (x, Ax, M, rank);
+		mulMatrix (A, x, condition, N, M, rank);
 	}
 
 	free (condition);
 	free (Ax);
-	printCol (x, N);
 }
 
 int main (int argc, char *argv[]) {
 	int size, rank;
-	MPI_Init(&argc, &argv);               // Инициализация MPI
-	MPI_Comm_size(MPI_COMM_WORLD, &size); // Получение числа процессов
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Получение номера процесса
+	MPI_Init (&argc, &argv);               // Инициализация MPI
+	MPI_Comm_size (MPI_COMM_WORLD, &size); // Получение числа процессов
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank); // Получение номера процесса
 	const size_t N = 8;
 	const size_t M = N / size;
 	const double EPSILON = 0.00001;
 	double *A = NULL;
-	A = (double *) malloc (N * M * sizeof (double));
+	A = (double *)malloc (N * M * sizeof (double));
 	double *b = NULL;
-	b = (double *) malloc (N * sizeof (double));
+	b = (double *)malloc (N * sizeof (double));
 	double *x = NULL;
-	x = (double *) malloc (N * sizeof (double));
+	x = (double *)malloc (N * sizeof (double));
 
 	if (x == NULL || b == NULL || A == NULL) {
 		free (A);
 		free (b);
 		free (x);
 		printf ("memory allocation error");
-		MPI_Finalize(); // Завершение работы MPI
+		MPI_Finalize (); // Завершение работы MPI
 		return 0;
 	}
 
@@ -120,15 +126,31 @@ int main (int argc, char *argv[]) {
 				A[i * N + j] = 1;
 			}
 		}
+	}
+
+	for (size_t i = 0; i < N; ++i) {
 		x[i] = 0;
 		b[i] = N + 1;
 	}
 
-	simpleIterationMethod (A, b, x, N, EPSILON);
+	gettimeofday(&tv1, NULL);
+	simpleIterationMethod (A, b, x, N, M, rank, EPSILON);
+	gettimeofday(&tv2, NULL);
+	if (rank == 0) {
+		printCol (x, N);
+	}
+	double dt_sec = (tv2.tv_sec - tv1.tv_sec);
+	double dt_usec = (tv2.tv_usec - tv1.tv_usec);
+	double dt = dt_sec + 1e-6*dt_usec;
+	double maxdt;
+	MPI_Allreduce(&dt, &maxdt, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+	if (rank == 0) {
+		printf("\nTime diff %lf\n", maxdt);
+	}
 
 	free (A);
 	free (b);
 	free (x);
-	MPI_Finalize(); // Завершение работы MPI
+	MPI_Finalize (); // Завершение работы MPI
 	return 0;
 }
